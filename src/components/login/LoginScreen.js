403b037cdfe589052a31de826ed705adc798aa2c
Carlos Morales <carlosjosuemn98@gmail.com>
2020-10-31 23:51:58 -0500

    se mejoro el login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,27 +1,59 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../auth/AuthContext';
 import { types } from '../../types/types';
+import './LoginScreen.css';
 
 export const LoginScreen = () => {
+    const [values, setValue] = useState({ input: '', error: false });
+    const { input, error } = values;
     const { dispatch } = useContext(AuthContext);
 
-    const handleLogin = () => {
-        const actionAuth = {
-            type: types.login,
-            payload: {
-                name: 'Carlos',
-            },
-        };
+    const handleLogin = (e) => {
+        e.preventDefault();
 
-        dispatch(actionAuth);
+        if (input.trim().length > 0) {
+            setValue({
+                ...values,
+                error: false,
+            });
+
+            const actionAuth = {
+                type: types.login,
+                payload: {
+                    name: input,
+                },
+            };
+
+            dispatch(actionAuth);
+        } else {
+            setValue({
+                ...values,
+                error: true,
+            });
+        }
     };
+
+    const handleInput = ({ target: { value } }) => {
+        setValue({
+            ...values,
+            input: value,
+        });
+    };
+
     return (
-        <div className='container mt-5'>
-            <h1>Login Screen</h1>
-            <hr />
-            <button className='btn btn-info' onClick={handleLogin}>
-                Ingresar
-            </button>
+        <div id='login'>
+            <div className='login-container'>
+                <h1>Login</h1>
+                <hr />
+                <form onSubmit={handleLogin}>
+                    <div className='form-group'>
+                        <label>Ingresa Tu Nombre</label>
+                        <input type='text' className='form-control' value={input} onChange={handleInput} />
+                        {error && <span className='text-danger d-block mt-2'>Debes ingresar tu nombre.</span>}
+                    </div>
+                    <button className='btn btn-info'>Ingresar</button>
+                </form>
+            </div>
         </div>
     );
 };
